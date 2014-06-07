feb576f85e160ab039bfbe888041a93fa2bc15c3
SKART1 <art.enikoff@gmail.com>
2014-06-08 00:06:09 +0400

    SlaveStarterImplemented

--- SlaveStarterPrj/SlaveStarterPrj.cc
+++ SlaveStarterPrj/SlaveStarterPrj.cc
@@ -1,7 +1,51 @@
 #include <cstdlib>
 #include <iostream>
+#include <stdio.h>
+
+#include "../SlavePrj/Slave.hpp"
+#include "../ServerPrj/ServerPrj.hpp"
+
+int pareseServerInfoFile(std::string pathToInfoFile, ArgSlaveStruct *argSlaveStruct){
+	FILE *filePointer;
+	int result=0;
+	if((filePointer=fopen(pathToInfoFile.c_str(),"r"))==NULL){
+		std::cerr<<"[ERROR]: Can not server info file to "<<pathToInfoFile<<std::endl;
+		return -1;
+	}
+	else{
+		result=result+fscanf(filePointer, "SERVER_NODE_NAME: %d\n", argSlaveStruct->serverNodeName);
+		result=result+fscanf(filePointer, "SERVER_PID: %d\n", &argSlaveStruct->pid);
+		result=result+fscanf(filePointer, "CHID_TASKS_FOR_SLAVES: %d\n", &argSlaveStruct->chidTasks);
+		result=result+fscanf(filePointer, "CHID_RESULTS_FOR_CLIENTS: %d\n", &argSlaveStruct->chidResults);
+		fclose(filePointer);
+		if(result!=4){
+			std::cerr<<"[ERROR]: Wrong server info format"<<std::endl;
+			return -1;
+		}
+		return 0;
+	}
+
+
+}
+
+
 
 int main(int argc, char *argv[]) {
+	/*Slaves*/
+	ArgSlaveStruct *argSlaveStruct = new ArgSlaveStruct ;
+
+	if(argc<2){
+		std::cerr<<"[INFO]: No path given. Using default serv.serv"<<std::endl;
+		pareseServerInfoFile("serv.serv", argSlaveStruct);
+	}
+	else{
+		pareseServerInfoFile(argv[1], argSlaveStruct);
+	}
+
+
+	Slave(argSlaveStruct);
+
+
 	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
 	return EXIT_SUCCESS;
 }
